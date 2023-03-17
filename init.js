c67ac31f4d6806022cf04de10e851acd8992a433
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-03-17 11:55:56 +0800

    music

--- init.js
+++ init.js
@@ -1,110 +1,18 @@
-class OverworldMap {
-  constructor(config) {
-    this.overworld = null;
-    this.gameObjects = config.gameObjects;
-    this.cutsceneSpaces = config.cutsceneSpaces || {};
-    this.walls = config.walls || {};
 
-    this.lowerImage = new Image();
-    this.lowerImage.src = config.lowerSrc;
-
-    this.upperImage = new Image();
-    this.upperImage.src = config.upperSrc;
-
-    this.isCutscenePlaying = false;
-    this.isPaused = false;
-  }
-
-  drawLowerImage(ctx) {
-    ctx.drawImage(this.lowerImage, 0, 0);
-  }
-
-  drawUpperImage(ctx) {
-    ctx.drawImage(this.upperImage, 0, 0);
-  }
-
-  isSpaceTaken(currentX, currentY, direction) {
-    const { x, y } = utils.nextPosition(currentX, currentY, direction);
-    return this.walls[`${x},${y}`] || false;
-  }
-
-  mountObjects() {
-    Object.keys(this.gameObjects).forEach((key) => {
-      let object = this.gameObjects[key];
-      object.id = key;
-
-      //TODO: determine if this object should actually mount
-      object.mount(this);
-    });
-  }
-
-  async startCutscene(events) {
-    this.isCutscenePlaying = true;
-
-    for (let i = 0; i < events.length; i++) {
-      const eventHandler = new OverworldEvent({
-        event: events[i],
-        map: this,
-      });
-      const result = await eventHandler.init();
-      if (result === "LOST_BATTLE") {
-        break;
-      }
-    }
-    this.isCutscenePlaying = false;
-  }
-
-  checkForActionCutscene() {
-    const hero = this.gameObjects["hero"];
-    //const nextCoords = utils.nextPosition(hero.x, hero.y, hero.direction);
-    const match = Object.values(this.gameObjects).find((object) => {
-      return `${object.x},${object.y}` === `${nextCoords.x},${nextCoords.y}`;
-    });
-    if (!this.isCutscenePlaying && match && match.talking.length) {
-      const relevantScenario = match.talking.find((scenario) => {
-        return (scenario.required || []).every((sf) => {
-          return playerState.storyFlags[sf];
-        });
-      });
-      relevantScenario && this.startCutscene(relevantScenario.events);
-    }
-  }
-
-  checkForFootstepCutscene() {
-    const hero = this.gameObjects["hero"];
-    const match = this.cutsceneSpaces[`${hero.x},${hero.y}`];
-    if (!this.isCutscenePlaying && match) {
-      this.startCutscene(match[0].events);
-    }
-  }
-
-  addWall(x, y) {
-    this.walls[`${x},${y}`] = true;
-  }
-  removeWall(x, y) {
-    delete this.walls[`${x},${y}`];
-  }
-  moveWall(wasX, wasY, direction) {
-    this.removeWall(wasX, wasY);
-    const { x, y } = utils.nextPosition(wasX, wasY, direction);
-    this.addWall(x, y);
-  }
-}
-
-const sleep = document?.querySelector(".sleep");
+//const sleep = document?.querySelector(".sleep");
 const music = document?.querySelector(".music");
 
-sleep?.addEventListener("mousedown", clicked1, false);
+//sleep?.addEventListener("mousedown", clicked1, false);
 music?.addEventListener("mousedown", clicked2, false);
 
 // city?.addEventListener("mousedown", clicked3, false);
 const soundPlayer = new Audio("/images/characters/people/music.mp3");
-function clicked1(e) {
-  e.preventDefault();
-  alert("Music stopped");
-  soundPlayer.pause();
-  soundPlayer.currentTime = 0;
-}
+// function clicked1(e) {
+//   e.preventDefault();
+//   alert("Music stopped");
+//   soundPlayer.pause();
+//   soundPlayer.currentTime = 0;
+// }
 function clicked2(e) {
   e.preventDefault();
   alert("Music started");
@@ -112,8 +20,8 @@ function clicked2(e) {
   soundPlayer.play();
   soundPlayer.loop = true;
 }
-
-const city = document?.querySelector(".city");
+soundPlayer.play();
+const sleep = document?.querySelector(".sleep");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button");
 
@@ -127,7 +35,7 @@ function windowOnClick(event) {
   }
 }
 
-city.addEventListener("click", toggleModal);
+sleep.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
@@ -169,17 +77,4 @@ cityDay.addEventListener("click", toggleModal3);
 closeButton3.addEventListener("click", toggleModal3);
 window.addEventListener("click", windowOnClick3);
 
-window.OverworldMaps = {
-  Kitchen: {
-    id: "Kitchen",
-    lowerSrc: "",
-    upperSrc: "",
-    gameObjects: {
-      hero: new Person({
-        isPlayerControlled: true,
-        x: utils.withGrid(30),
-        y: utils.withGrid(50),
-      }),
-    },
-  },
-};
+
